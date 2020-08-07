e26a36d15551176ce0caee0d24d6bbff91265603
yohni <yohni.123@gmail.com>
2020-08-07 10:41:18 +0700

    Create max container

---
+++ components/layout/BaseContainer.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled, { ThemeProvider } from 'styled-components';
+import PropTypes from 'prop-types';
+import palletes from '../../theme';
+
+import BottomNavigation from '../BottomNavigation';
+
+const Wrapper = styled.div`
+  width: 100%;
+  background: ${(props) => props.theme.GreyBasic};
+`;
+
+const Container = styled.div`
+  max-width: 480px;
+  background: #fff;
+  margin: 0 auto;
+`;
+
+const BaseContainer = ({ children }) => {
+  return (
+    <ThemeProvider theme={palletes}>
+      <Wrapper>
+        <Container>
+          {children}
+          <BottomNavigation />
+        </Container>
+      </Wrapper>
+    </ThemeProvider>
+  );
+};
+
+BaseContainer.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default BaseContainer;
