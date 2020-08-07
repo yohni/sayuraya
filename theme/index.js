a077f738c43b4701f0419b990dafa8a09d6c56d4
yohni <yohni.123@gmail.com>
2020-08-07 10:42:31 +0700

    Add pallete

---
+++ theme/index.js
@@ -0,0 +1,12 @@
+const palletes = {
+  GreenPrimary: '#58A571',
+  GreenSecondary: '#95CF94',
+  GreenTersier: '#C2E17D',
+  GreyBasic: '#E8E8E8',
+  GreyDark: '#B0B0B0',
+  OrangeWood: '#EF833B',
+  YellowSmooth: '#FCE655',
+  YellowCream: '#F3F1B4',
+};
+
+export default palletes;
