c9fd21915f0e76f88a80a4f91747c92bfc427efb
yohni <yohni.123@gmail.com>
2020-08-10 15:13:06 +0700

    Add new color

--- theme/index.js
+++ theme/index.js
@@ -1,9 +1,11 @@
 const palletes = {
+  BlackMain: '#1C1211',
   GreenPrimary: '#58A571',
   GreenSecondary: '#95CF94',
   GreenTersier: '#C2E17D',
   GreyBasic: '#E8E8E8',
   GreyDark: '#B0B0B0',
+  GreyText: '#636A75',
   OrangeWood: '#EF833B',
   YellowSmooth: '#FCE655',
   YellowCream: '#F3F1B4',
