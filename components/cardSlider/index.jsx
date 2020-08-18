53fcb4d994b7d2a8eaedccd0e2a37e9cfdc2361c
yohni <yohni.123@gmail.com>
2020-08-18 08:48:46 +0700

    Implement react slick

---
+++ components/cardSlider/index.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import Slider from 'react-slick';
+import propTypes from 'prop-types';
+
+const CardSlider = ({ children }) => {
+  const responsive = [
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 320,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+  ];
+  return (
+    <Slider
+      arrows={false}
+      className="w-100"
+      dots={false}
+      infinite={false}
+      speed={500}
+      slidesToShow="3"
+      slidesToScroll="1"
+      responsive={responsive}
+    >
+      {children}
+    </Slider>
+  );
+};
+
+CardSlider.propTypes = {
+  children: propTypes.oneOfType([propTypes.arrayOf(propTypes.node), propTypes.node]).isRequired,
+};
+
+export default CardSlider;
