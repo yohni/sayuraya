00194c0410d52fe89731082e16108612be607745
yohni <yohni.123@gmail.com>
2020-08-18 08:50:27 +0700

    Implement carousel

--- components/SlideShow/index.jsx
+++ components/SlideShow/index.jsx
@@ -3,6 +3,20 @@ import styled from 'styled-components';
 import { Carousel, CarouselItem, CarouselControl } from 'reactstrap';
 import PropTypes from 'prop-types';
 
+const SlideImage = styled.img`
+  height: 160px;
+  width: 100%;
+  object-fit: cover;
+
+  @media (min-width: 480px) {
+    height: 240px;
+  }
+`;
+
+const SlideControl = styled(CarouselControl)`
+  opacity: 0;
+`;
+
 const SlideShow = (props) => {
   const [activeIndex, setActiveIndex] = useState(0);
   const [animating, setAnimating] = useState(false);
@@ -19,20 +33,6 @@ const SlideShow = (props) => {
     const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
     setActiveIndex(nextIndex);
   };
-
-  const SlideImage = styled.img`
-    height: 160px;
-    width: 100%;
-    object-fit: cover;
-
-    @media (min-width: 480px) {
-      height: 240px;
-    }
-  `;
-
-  const SlideControl = styled(CarouselControl)`
-    opacity: 0;
-  `;
 
   const slides = items.map((item) => (
     <CarouselItem
