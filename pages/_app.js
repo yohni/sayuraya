288373297a8ebee3557a034e44f393a60636ab03
yohni <yohni.123@gmail.com>
2020-08-18 08:51:59 +0700

    Import react slick

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
