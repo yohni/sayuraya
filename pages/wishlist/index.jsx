ac4b85daeb060799108d3de8a2764bea7d2b8ff6
yohni <yohni.123@gmail.com>
2020-08-27 13:31:19 +0700

    refactor: 💡 delete directory

--- pages/wishlist/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import BaseContainer from '../../components/layout/BaseContainer';
-
-const Wishlist = () => {
-  return <BaseContainer>Wishlist</BaseContainer>;
-};
-
-export default Wishlist;
